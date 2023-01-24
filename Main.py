f33c74116d9d077e1898a35cf70df8aef5942b71
APXP1 <118368546+APXP1@users.noreply.github.com>
2023-01-24 09:36:54 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,7 +21,7 @@ p1 = 200
 pp1 = -200
 
 p2 = 400
-pp2= -200
+pp2 = -200
 
 p3 = 600
 pp3 = -200
@@ -38,7 +38,7 @@ asteroida = pygame.image.load('asteroida.png')
 postac = pygame.image.load("postać.png")
 tlo = pygame.image.load('tło.webp')
 predkosc = 7
-punkty
+punkty = 0
 run = True
 while run:
     for event in pygame.event.get():
@@ -56,15 +56,15 @@ while run:
         x -= predkosc
     if keys[pygame.K_RIGHT]:
         x += predkosc
-    #zapętlony wyświetlany pixel przemieszczający się w dół
+    # zapętlony wyświetlany pixel przemieszczający się w dół
     while py == 700:
-        #ustawienie nowego położenia y
+        # ustawienie nowego położenia y
         py = 0
-        py1=0
-        py2=0
-        #losowanie położenia x
+        py1 = 0
+        py2 = 0
+        # losowanie położenia x
         px = random.choice([0, 550])
-    #zmenianie poziomu y różnych rzeczy (py to pixele, pp asteroidy)
+    # zmenianie poziomu y różnych rzeczy (py to pixele, pp asteroidy)
     py += 4
     py1 += 8
     py2 += 16
@@ -75,80 +75,90 @@ while run:
     pp3 += 7
     pp4 += 10
     pp5 += 5
-    #resetowanie położenia y asteroidy
-    while pp>720:
-        pp =-200
-        
-    while pp1>720:
-        pp1=-200
+    # resetowanie położenia y asteroidy
+    while pp > 720:
+        pp = -200
+
+    while pp1 > 720:
+        pp1 = -200
         punkty += 1
-    while pp2>720:
-        pp2=-200
+    while pp2 > 720:
+        pp2 = -200
         punkty += 1
-    while pp3>720:
-        pp3=-200
+    while pp3 > 720:
+        pp3 = -200
         punkty += 1
-    while pp4>720:
-        pp4=-200
+    while pp4 > 720:
+        pp4 = -200
         punkty += 1
-    while pp5>720:
-        pp5=-200
+    while pp5 > 720:
+        pp5 = -200
         punkty += 1
-    #koniec gry (w przypadku kolizji)
-    while (p+130>x and p+130<x+140 and pp+102==y) or (p+30>x and p+30<x+140 and pp+102==y) or (p+210>x and p+210<x+140 and pp+102==y):
+    # koniec gry (w przypadku kolizji)
+    while (p + 130 > x and p + 130 < x + 140 and pp + 102 == y) or (
+            p + 30 > x and p + 30 < x + 140 and pp + 102 == y) or (p + 210 > x and p + 210 < x + 140 and pp + 102 == y):
         font = pygame.font.SysFont('comicsans', 30)
-        label = font.render('Game Over ', 1, (255, 255, 255))
+        label = font.render('Game Over, twój wynik to:  ',punkty, 1, (255, 255, 255))
         win.blit(label, (100, 425))
         pygame.display.update()
         time.sleep(1.5)
         pygame.quit()
-    while (p1+130>x and p1+130<x+140 and pp1+102==y) or (p1+30>x and p1+30<x+140 and pp1+102==y) or (p1+210>x and p1+210<x+140 and pp1+102==y):
+    while (p1 + 130 > x and p1 + 130 < x + 140 and pp1 + 102 == y) or (
+            p1 + 30 > x and p1 + 30 < x + 140 and pp1 + 102 == y) or (
+            p1 + 210 > x and p1 + 210 < x + 140 and pp1 + 102 == y):
         font = pygame.font.SysFont('comicsans', 30)
-        label = font.render('Game Over ', 1, (255, 255, 255))
+        label = font.render('Game Over, twój wynik to: ',punkty, 1, (255, 255, 255))
         win.blit(label, (300, 425))
         pygame.display.update()
         time.sleep(1.5)
         pygame.quit()
-    while (p2+130>x and p2+130<x+140 and pp2+100==y) or (p2+30>x and p2+30<x+140 and pp2+100==y) or (p2+210>x and p2+210<x+140 and pp2+100==y):
+    while (p2 + 130 > x and p2 + 130 < x + 140 and pp2 + 100 == y) or (
+            p2 + 30 > x and p2 + 30 < x + 140 and pp2 + 100 == y) or (
+            p2 + 210 > x and p2 + 210 < x + 140 and pp2 + 100 == y):
         font = pygame.font.SysFont('comicsans', 30)
-        label = font.render('Game Over ', 1, (255, 255, 255))
+        label = font.render('Game Over, twój wynik to: ',punkty, 1, (255, 255, 255))
         win.blit(label, (500, 425))
         pygame.display.update()
         time.sleep(1.5)
         pygame.quit()
-    while (p3+130>x and p3+130<x+140 and pp3+141==y) or (p3+30>x and p3+30<x+140 and pp3+141==y) or (p3+210>x and p3+210<x+140 and pp3+141==y):
+    while (p3 + 130 > x and p3 + 130 < x + 140 and pp3 + 141 == y) or (
+            p3 + 30 > x and p3 + 30 < x + 140 and pp3 + 141 == y) or (
+            p3 + 210 > x and p3 + 210 < x + 140 and pp3 + 141 == y):
         font = pygame.font.SysFont('comicsans', 30)
-        label = font.render('Game Over ', 1, (255, 255, 255))
+        label = font.render('Game Over, twój wynik to: ',punkty, 1, (255, 255, 255))
         win.blit(label, (700, 425))
         pygame.display.update()
         time.sleep(1.5)
         pygame.quit()
-    while (p4+130>x and p4+130<x+140 and pp4+120==y) or (p4+30>x and p4+30<x+140 and pp4+120==y) or (p4+210>x and p4+210<x+140 and pp4+120==y):
+    while (p4 + 130 > x and p4 + 130 < x + 140 and pp4 + 120 == y) or (
+            p4 + 30 > x and p4 + 30 < x + 140 and pp4 + 120 == y) or (
+            p4 + 210 > x and p4 + 210 < x + 140 and pp4 + 120 == y):
         font = pygame.font.SysFont('comicsans', 30)
-        label = font.render('Game Over ', 1, (255, 255, 255))
+        label = font.render('Game Over, twój wynik to :',punkty, 1, (255, 255, 255))
         win.blit(label, (900, 425))
         pygame.display.update()
         time.sleep(1.5)
         pygame.quit()
-    while (p5+130>x and p5+130<x+140 and pp5+110==y) or (p5+30>x and p5+30<x+140 and pp5+110==y) or (p5+210>x and p5+210<x+140 and pp5+110==y):
+    while (p5 + 130 > x and p5 + 130 < x + 140 and pp5 + 110 == y) or (
+            p5 + 30 > x and p5 + 30 < x + 140 and pp5 + 110 == y) or (
+            p5 + 210 > x and p5 + 210 < x + 140 and pp5 + 110 == y):
         font = pygame.font.SysFont('comicsans', 30)
-        label = font.render('Game Over ', 1, (255, 255, 255))
+        label = font.render('Game Over, twój wynik to: ',punkty, 1, (255, 255, 255))
         win.blit(label, (1100, 425))
         pygame.display.update()
         time.sleep(1.5)
         pygame.quit()
-    #bariera
-    while x<0:
-        x=0
-    while x>1150:
-        x=1150
+    # bariera
+    while x < 0:
+        x = 0
+    while x > 1150:
+        x = 1150
 
-
-    #wyświetlanie
+    # wyświetlanie
     win.blit(tlo, (0, 0))
     win.blit(postac, (x, y))
-    win.blit(asteroida, (p,pp))
-    win.blit(asteroida,(p1,pp1))
+    win.blit(asteroida, (p, pp))
+    win.blit(asteroida, (p1, pp1))
     win.blit(asteroida, (p2, pp2))
     win.blit(asteroida, (p3, pp3))
     win.blit(asteroida, (p4, pp4))
